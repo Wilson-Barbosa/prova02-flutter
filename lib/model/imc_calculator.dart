404e4340c4db3ee51e0f3af8d43dace0aee01fa4
Wilson-Barbosa <wilsond_barbosa@outlook.com>
2024-11-29 22:49:25 -0300

    adds images and model for imc calculator and user

---
+++ lib/model/imc_calculator.dart
@@ -0,0 +1,31 @@
+class ImcCalculator {
+  double weight;
+  double height;
+
+  ImcCalculator({required this.weight, required this.height});
+
+  double calculateImc() {
+    return weight / (height * height);
+  }
+
+  String returnMessage() {
+    double imc = calculateImc();
+    String message = "";
+
+    if (imc < 18.5) {
+      message = "Abaixo do peso";
+    } else if (imc >= 18.5 || imc < 25) {
+      message = "Seu peso é normal";
+    } else if (imc >= 25 || imc < 30) {
+      message = "Possível sobrepeso";
+    } else if (imc >= 30 || imc < 35) {
+      message = "Obsidade grau 1";
+    } else if(imc >=35 || imc < 40) {
+      message = "Obesidade grau 2";
+    } else if(imc >= 40 ) {
+      message = "Obesidade grau 3";
+    }
+    
+    return message;
+  }
+}
---
+++ lib/model/user_model.dart
@@ -0,0 +1,12 @@
+class UserModel {
+
+  String name;
+  String email;
+  String password;
+
+  // full args constructor
+  UserModel({ required this.name, required this.email, required this.password });
+
+
+
+}
